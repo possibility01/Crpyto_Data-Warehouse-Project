2cf717ee0aff63fb6da0eb957cbf31b0ae90ab9b
Akeelah <idristobi86@gmail.com>
2025-12-09 01:38:08 +0100

    added init database and api code

--- Api_connecting.py
+++ Api_connecting.py
@@ -1,76 +1,35 @@
 import requests
-import json
 import pandas as pd
 
-def connecting_coin_market_api():
+def connecting_coin_market_api(per_page):
     url = "https://api.coingecko.com/api/v3/coins/markets"
+
     params = {
-        "vs_currency": "usd",       # currency to get prices in
-        "order": "market_cap_desc", # order by market cap descending
-        "per_page": 5,              # number of coins per page
-        "page": 1,                  # page number
-        "sparkline": False          # whether to include sparkline data
+        "vs_currency": "usd",
+        "order": "market_cap_desc",
+        "per_page": per_page,
+        "page": 1,
+        "sparkline": False
     }
     
     try:
-        # Make the request with parameters
-        response = requests.get(url, params=params, timeout=10)  # timeout in seconds
-        response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+        response = requests.get(url, params=params, timeout=10)
+        response.raise_for_status()
+        print(f'The response code is {response}')
+        data = response.json()  
         
-        # Convert response to JSON
-        data_ = response.json()
-        return data_
-    
+        print(f"Fetched top {len(data)} coins by marketcap successfully")
+        return data
 
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-    
-    return None  # return None if there was an error
-
-data = connecting_coin_market_api()
-data
+        print(f"Error: {e}")
+        return None
 
 
-def process_coin_data(coin_dict):
-  coin_market_result = {}
+# Fetch top 50 coins
+result = connecting_coin_market_api(per_page=50)
 
-  coin_market_result["id"] = coin_dict["id"]
-  coin_market_result["symbol"] = coin_dict["symbol"]
-  coin_market_result["name"] = coin_dict["name"]
-  coin_market_result["current_price"] = coin_dict["current_price"]
-  coin_market_result["market_cap"] = coin_dict["market_cap"]
-  coin_market_result["market_cap_rank"] = coin_dict["market_cap_rank"]
-  coin_market_result["fully_diluted_valuation"] = coin_dict["fully_diluted_valuation"]
-  coin_market_result["total_volume"] = coin_dict["total_volume"]
-  coin_market_result["high_24h"] = coin_dict["high_24h"]
-  coin_market_result["low_24h"] = coin_dict["low_24h"]
-  coin_market_result["price_change_24h"] = coin_dict["price_change_24h"]
-  coin_market_result["price_change_percentage_24h"] = coin_dict["price_change_percentage_24h"]
-  coin_market_result["market_cap_change_24h"] = coin_dict["market_cap_change_24h"]
-  coin_market_result["market_cap_change_percentage_24h"] = coin_dict["market_cap_change_percentage_24h"]
-  coin_market_result["circulating_supply"] = coin_dict["circulating_supply"]
-  coin_market_result["total_supply"] = coin_dict["total_supply"]
-  coin_market_result["max_supply"] = coin_dict["max_supply"]
-  coin_market_result["ath"] = coin_dict["ath"]
-  coin_market_result["ath_change_percentage"] = coin_dict["ath_change_percentage"]
-  coin_market_result["ath_date"] = coin_dict["ath_date"]
-  coin_market_result["atl"] = coin_dict["atl"]
-  coin_market_result["atl_change_percentage"] = coin_dict["atl_change_percentage"]
-  coin_market_result["atl_date"] = coin_dict["atl_date"]
-  coin_market_result["roi"] = coin_dict["roi"]
-  coin_market_result["last_updated"] = coin_dict["last_updated"]
-
-  return coin_market_result
-
-# Process and print the result
-processed_coin_data = []
-
-for coin in data:
-    processed_coin = process_coin_data(coin)
-    processed_coin_data.append(processed_coin)
-processed_coin_data
-
-import pandas as pd
-
-df = pd.DataFrame(processed_coin_data)
+# Convert list to DataFrame
+df = pd.DataFrame(result)
+df['last data date'] = pd.to_datetime('today').strftime('%Y-%m-%d %H:%M:%S')
 print(df)
---
+++ init_database.py
@@ -0,0 +1,104 @@
+import pypyodbc as odbc
+import sys
+
+
+driver_name  = "SQL SERVER"  
+server_name = r"Akeelah\SQLEXPRESS"
+database_name = "master"
+new_database = "Crypto_DataWarehouse"
+schemas = ['bronze','silver','gold'] 
+
+def connect_sql_server (driver,server,database):
+    
+
+    try:
+        print ('>>>creating connection........................🔄🔄🔄🔄')
+
+        connection = odbc.connect(
+        f"DRIVER={{{driver}}};"
+        f"SERVER={server};"
+        f"DATABASE={database};"
+        "Trusted_Connection=yes;" )
+
+        print("Connected successfully!.......✅✅✅")
+
+        print("--------------------------------------------------------------")
+        return connection
+
+    except odbc.Error as e:
+        print(f'❌❌📛❌📛❌Error encounted while trying to connect to SQL SERVER: {e}')
+        sys.exit(1)
+
+def drop_database_if_exist(cursor,dbname):
+
+
+    try:
+        print ('dropping database if existed 🚮🚮🚮')
+        drop_database = f"""
+                IF EXISTS (SELECT 1 FROM sys.databases WHERE name = '{dbname}')
+                BEGIN 
+                    ALTER DATABASE {dbname} SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+                    DROP DATABASE {dbname};
+                END
+            """
+        cursor.execute(drop_database)
+
+        print("Existing DataWarehouse dropped (if existed) successfully🚮🚮🚮")
+        print("--------------------------------------------------------------")
+    except odbc.Error as e:
+        print(f'❌❌📛❌📛❌Error encounted while trying to drop database: {e}')
+        sys.exit(1)
+
+def create_datawarehouse(cursor,dbname):
+    print(f'creating {dbname}...........................🔃🔃🔃🔃')
+
+    cursor.execute(f'CREATE DATABASE [{dbname}]')
+    print(f' {dbname} created successfully........................✅✅✅')
+    print("--------------------------------------------------------------")
+
+def create_schemas(cursor,schemas):
+
+        print(f'creating {schemas}...........................🔃🔃🔃🔃')
+
+        for schema in schemas:
+            cursor.execute(f'CREATE SCHEMA {schema}')
+            print (f'{schema} schema created......✅✅✅')
+        print("--------------------------------------------------------------")
+
+def main():
+
+    connection = connect_sql_server (driver_name,server_name,database_name)
+
+    try:
+
+        cursor = connection.cursor()
+
+        #drop database if exit
+        drop_database_if_exist(cursor,new_database)
+        cursor.commit()
+
+        #create database
+        create_datawarehouse(cursor,new_database)
+        cursor.commit()
+
+        cursor.execute(f'USE [{new_database}]')
+        print(f'{new_database} IN USE')
+
+        #create schema
+        create_schemas(cursor,schemas)
+        cursor.commit()
+
+    except odbc.Error as e:
+        print(f'SQL SERVER ERROR:{e}')
+
+    finally:
+        cursor.close()
+        connection.close()
+        print('connection closed............')
+
+if __name__ == "__main__":
+    main()
+
+
+
+
