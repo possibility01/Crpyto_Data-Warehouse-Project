b841f151a3957dc7336f389ed19a2356b518dc31
Akeelah <idristobi86@gmail.com>
2025-12-29 15:49:17 +0100

    finished the silver load query except coinmarket

--- silver/load_silver_layer.py
+++ silver/load_silver_layer.py
@@ -1,6 +1,7 @@
 import pypyodbc as odbc
 import pandas as pd
 import sys
+import time
 
 driver_name  = "SQL Server"
 server_name = r"Akeelah\SQLEXPRESS"
@@ -66,9 +67,7 @@ def querying_bronze_layer(driver,server,database):
         print(f'Error querying top 50 coin {tables}: {e}......................❌❌❌')
         return None 
     
-    finally:
-        connection.close()
-        print("queried all the tables and connection closed successfully...........✅✅✅")
+    
 
 
 def cleaning_table(table_data):
@@ -91,6 +90,9 @@ def cleaning_table(table_data):
 
         basic_info = table_data['bronze.coin_basic_info']
         basic_info.loc[:, :] = basic_info.fillna('Not available')
+
+        social_links = table_data["bronze.coin_links_social"]
+        social_links.loc[:,:] =social_links.fillna('Not available')
     
     except Exception as e:
         print(f'Error while trying to clean :{e}')
@@ -109,6 +111,147 @@ def cleaning_table(table_data):
     return table_data
 
            
+def load_data(table_data,driver,server,database):
+
+    print('Loading the data into the tables.....................................🔄🔄🔄')
+
+    
+
+    table_config = {
+    "silver.coin_basic_info": {
+        "id_col": "id",
+        "insert_cols": [
+            "id", "symbol", "name", "preview_listing",
+            "description", "country_origin",
+            "genesis_date", "last_updated"
+        ],
+        "source_df": table_data['bronze.coin_basic_info']
+    },
+
+    "silver.coin_categories_info": {
+        "id_col": "coin_id",
+        "insert_cols": ["coin_id", "category"],
+        "source_df": table_data["bronze.coin_categories_info"]
+    },
+
+    "silver.candle_historical_data": {
+        "id_col": "coin_id",
+        "insert_cols": ['[timestamp]', '[open]', '[high]', '[low]', '[close]',
+                'coin_id', 'coin_name', '[datetime]'
+        ],
+        "source_df": table_data['bronze.candle_historical_data']
+    },
+
+#     'silver.coin_market': {
+#         "id_col": "id",
+#         "insert_cols": [
+#             "id", "symbol", "name", "image",
+#             "current_price", "market_cap",
+#             "market_cap_rank", "fully_diluted_valuation" ,
+#             "total_volume","high_24h",  "low_24h","price_change_24h","price_change_percentage_24h","market_cap_change_24h" ,
+#   "market_cap_change_percentage_24h",
+#   "circulating_supply", "total_supply", "max_supply",
+#             "ath", "ath_change_percentage",
+#             "ath_date", "atl"  ,"atl_change_percentage", "atl_date", "last_updated", "last_data_date",
+           
+#         ],
+#         "source_df":table_data['bronze.coin_market'] 
+#     },
+								
+ 
+        
+
+    "silver.coin_platform_info": {
+        "id_col": "coin_id",
+        "insert_cols": ["coin_id", "platform", "contract_address"],
+        "source_df": table_data['bronze.coin_platform_info']
+    },
+    "silver.coin_developer_info": {
+        "id_col": "coin_id",
+        "insert_cols": [
+            "forks", "stars", "subscribers", "total_issues",
+            "pull_requests_merged", "pull_request_contributors",
+            "commit_count_4_weeks", "coin_id"
+        ],
+        "source_df": table_data['bronze.coin_developer_info']
+    },
+
+    "silver.coin_links_social": {
+        "id_col": "coin_id",
+        "insert_cols": [
+            'coin_id', 'snapshot_url', 'twitter_screen_name',
+     'facebook_username', 'subreddit_url'
+
+        ],
+        "source_df": table_data["bronze.coin_links_social"]
+        
+        },
+
+    "silver.coin_links_repo": {
+        "id_col": "coin_id",
+        "insert_cols": [
+            'coin_id', 'repo_type', 'url'
+
+        ],
+        "source_df": table_data['bronze.coin_links_repo']
+        
+        },
+    "silver.coin_link_all_info": {
+        "id_col": "coin_id",
+        "insert_cols": [
+            'coin_id', 'url', 'link_type'
+
+        ],
+        "source_df": table_data['bronze.coin_link_all_info']
+        
+        }
+
+
+}
+    try:
+         
+        connection = connect_sql_server(driver,server,database)
+        cursor = connection.cursor()
+        cursor.fast_executemany = True
+        for table, conf in table_config.items():
+            source_df = conf['source_df']
+            id_col = conf['id_col']
+            insert_cols = conf['insert_cols']
+            db_cols = conf.get('db_cols', insert_cols)
+
+
+            placeholders = ",".join("?" * len(insert_cols))
+            params = list(source_df.itertuples(index=False, name=None))
+
+            insert_sql = f"""
+                            INSERT INTO {table} ({",".join(db_cols)})
+                            VALUES ({placeholders})
+                        """
+            start_time = time.time()         
+            try:
+                            cursor.executemany(insert_sql, params)
+                            connection.commit()
+                            
+                            elapsed = time.time() - start_time
+                            print(f'{len(params)} rows inserted in {table}.................... in {elapsed:.2f} seconds')
+            except Exception as e:
+                            print(f'Error inserting into {table}: {e}............................❌❌❌')
+                            print(f'First row sample: {params[0] if params else "No data"}')
+                            raise
+        
+
+            except Exception as e:
+                print(f'error coin while trying to load data into {table}.....................❌❌❌')
+
+    finally:
+
+        cursor.close()
+        connection.close()
+        print(f'connection closed........................................🔴🔴')
+        print("\n✓ All operations completed!.....................................✅✅✅")
+        
+
+            
 
         
 
@@ -130,8 +273,7 @@ def main():
     )
 
     cleaning_table(bronze_data)
-  
-
+    load_data(bronze_data,driver_name,server_name,database_name)
 
         
 
